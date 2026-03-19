0348aa50a20944e835884601b8b0040e696bd52d
Peter <schoolgamer008@gmail.com>
2026-03-19 19:45:47 +0100

    Added moving depth background

--- nyan_cat.py
+++ nyan_cat.py
@@ -1,1 +1,61 @@
 # nyan_cat
+
+import pygame
+import random
+
+pygame.init()
+
+WIDTH, HEIGHT = 1280, 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("nyan_cat")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+stars = []
+
+for i in range(120):
+
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+
+    depth = random.choice([1, 2, 3])
+
+    if depth == 1:
+        speed = 1
+        size = 1
+
+    elif depth == 2:
+        speed = 2
+        size = 2
+        
+    else:
+        speed = 4
+        size = 3
+
+    stars.append([x, y, speed, size])
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    for star in stars:
+        star[0] -= star[2]
+
+        if star[0] < 0:
+            star[0] = WIDTH
+            star[1] = random.randint(0, HEIGHT)
+
+        pygame.draw.circle(screen, (255, 255, 255), (star[0], star[1]), star[3])
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
