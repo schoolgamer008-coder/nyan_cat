d1e93b352939b30454dd5ff9e7312b04e4502eb8
Peter <schoolgamer008@gmail.com>
2026-03-22 12:39:47 +0100

    Added game phases and score

--- nyan_cat.py
+++ nyan_cat.py
@@ -6,8 +6,9 @@ import random
 pygame.init()
 pygame.mixer.init()
 
+font = pygame.font.SysFont("timesnewroman", 36, bold=True)
+
 pygame.mixer.music.load("nyan_cat.mp3")
-pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.01)
 
 WIDTH, HEIGHT = 1280, 720
@@ -31,37 +32,95 @@ for i in range(120):
     depth = random.choice([1, 2, 3])
 
     if depth == 1:
-        speed = 0.2
+        speed = 0.1
         size = 1
 
     elif depth == 2:
-        speed = 0.4
+        speed = 0.2
         size = 2
         
     else:
-        speed = 0.8
+        speed = 0.4
         size = 3
 
     stars.append([x, y, speed, size])
 
 running = True
+game_state = "START"
+score = 0
+start_ticks = 0
 
 while running:
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+        if event.type == pygame.KEYDOWN:
 
-    for star in stars:
-        star[0] -= star[2]
+            if game_state == "START":
 
-        if star[0] < 0:
-            star[0] = WIDTH
-            star[1] = random.randint(0, HEIGHT)
+                if event.key == pygame.K_SPACE:
+                    game_state = "PLAYING"
+                    start_ticks = pygame.time.get_ticks()
+                    pygame.mixer.music.play(-1)
 
-        pygame.draw.circle(screen, (255, 255, 255), (star[0], star[1]), star[3])
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+            elif game_state == "PLAYING":
+
+                if event.key == pygame.K_ESCAPE:
+                    game_state = "START"
+                    pygame.mixer.music.stop()
+
+                if event.key == pygame.K_g: #ker še ni collisionov, da lahko sploh pridem tja
+                    game_state = "GAMEOVER"
+                    pygame.mixer.music.stop()
+
+            elif game_state == "GAMEOVER":
+
+                if event.key == pygame.K_SPACE:
+                    game_state = "PLAYING"
+                    start_ticks = pygame.time.get_ticks()
+                    pygame.mixer.music.play(-1)
+
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+    screen.fill((15, 12, 41))
+
+    if game_state == "START":
+        start_msg = font.render("PRESS SPACE TO START", True, (255, 255, 255))
+        exit_msg = font.render("PRESS ESC TO QUIT", True, (255, 255, 255))
+        screen.blit(start_msg, (WIDTH//2 - 500, HEIGHT//2 - 220))
+        screen.blit(exit_msg, (WIDTH//2 - 500, HEIGHT//2 - 160))
+
+    elif game_state == "PLAYING":
+
+        for star in stars:
+            star[0] -= star[2]
+
+            if star[0] < 0:
+                star[0] = WIDTH
+                star[1] = random.randint(0, HEIGHT)
+
+            pygame.draw.circle(screen, (255, 255, 255), (star[0], star[1]), star[3])
+
+        seconds = (pygame.time.get_ticks() - start_ticks) / 1000
+        score = int((seconds ** 1.1) * 10)
+
+        score_surface = font.render(f"NYAN SCORE: {score:,}", True, (255, 255, 255))
+        screen.blit(score_surface, (20, 20))
+        
+    elif game_state == "GAMEOVER":
+        msg = font.render(f"GAME OVER! FINAL SCORE: {score:,}", True, (255, 255, 255))
+        restart_msg = font.render("PRESS SPACE TO RESTART", True, (255, 255, 255))
+        exit_msg = font.render("PRESS ESC TO QUIT", True, (255, 255, 255))
+        screen.blit(msg, (WIDTH//2 - 500, HEIGHT//2 - 220))
+        screen.blit(restart_msg, (WIDTH//2 - 500, HEIGHT//2 - 160))
+        screen.blit(exit_msg, (WIDTH//2 - 500, HEIGHT//2 - 100))
 
     pygame.display.flip()
     clock.tick(FPS)
